63724df04f5a58506e19a4398a258f2bb0390fea
John Shiles <john.shiles@talentsconnect.com>
2025-09-22 13:11:57 +0200

    latest changes

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,13 +1,23 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
+	"stevearc/conform.nvim",
+	opts = {},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff", "pyright" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 1000,
+				lsp_format = "fallback",
+			},
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			}),
+		})
 	end,
 }
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,11 +2,17 @@
 -- Theme
 -- =================================================================
 return {
-   "folke/tokyonight.nvim",
-   lazy = false, -- make sure the theme is loaded first
-   priority = 1000, -- make sure to load this before all the other start plugins
-   config = function()
-      vim.cmd("colorscheme tokyonight-storm")
-   end,
+	"folke/tokyonight.nvim",
+	lazy = false, -- make sure the theme is loaded first
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		require("tokyonight").setup({
+			transparent = true, -- Enable transparent background
+			styles = {
+				sidebars = "transparent", -- Make sidebars transparent
+				floats = "transparent", -- Make floating windows transparent
+			},
+		})
+		vim.cmd("colorscheme tokyonight-storm")
+	end,
 }
-
